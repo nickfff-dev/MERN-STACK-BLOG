94048a960d4547ca658a852d136ffa007b21a878
nickfff-dev <hunchographics@gmail.com>
2022-01-23 01:41:50 +0300

    fullstackapp

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import Signup from './Signup';
+import Signin from './Signin';
+import Home from './Home';
+import Mainfaqss from "./Mainfaqss";
+
+import{BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    
+
+  }
+  render(){
+   
+    return ( 
+    
+      
+       
+        
+         <Router>
+           
+           <Routes>
+       
+        
+       <Route exact path="/faqs"  element={<Mainfaqss/>} /> 
+        <Route exact path="/"   element={<Home /> } />
+        <Route exact path="/signup"  element={<Signup />} />
+        <Route exact path="/signin"  element={<Signin />} />
+       </Routes>
+        </Router>
+      
+    
+    )
+  }
+}
+
+export default App;
